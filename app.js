1cc05f8c364a808fa5d771b0e051e6fa8ebb0c2e
Luis Garzon <luisgarzon90@yahoo.com>
2018-01-30 20:45:17 -0500

    created random name generation as well as updating the page

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
+// variables 
 const button = document.querySelector('#generateBtn');
 const firstNameEl = document.querySelector ('#first');
 const lastNameEl = document.querySelector('#last');
+const firstNames = [
+    'neo',
+    'morpheus',
+    'trinity'
+]
+const lastNames = [
+    'gorgonzola',
+    'cheddar',
+    'havariti'
+]
 
+
+// eventListeners
 button.addEventListener('click', generateName);
 
+// functions 
 function generateName(e) {
-    console.log(firstNameEl);
-    console.log(lastNameEl);
+    const rndLastName = randomChoice(lastNames);
+    const rndFirstName = randomChoice(firstNames);
+    firstNameEl.textContent = rndFirstName;
+    lastNameEl.textContent = rndLastName; 
 
+}
+function randomChoice(array){
+    const rndIndex = Math.floor(Math.random() * array.length);
+    return array[rndIndex];
 }
